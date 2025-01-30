5ee2f17006a91422803843065ebd67bc9b5ec7c9
Julian Salazar <julian.salazarlpz@proton.me>
2025-01-30 21:41:24 +0000

    Style changes to gauss-seidel.py

--- gauss-seidel.py
+++ gauss-seidel.py
@@ -1,10 +1,10 @@
-from typing import Callable
+from typing import Callable, List, Tuple
 from time import sleep
 
-type Row = tuple[float, float, float, float]
-type Matrix = tuple[Row, Row, Row]
-type Results = tuple[float, float, float]
-type Error = tuple[float, float, float]
+type Row = Tuple[float, float, float, float]
+type Matrix = Tuple[Row, Row, Row]
+type Results = Tuple[float, float, float]
+type Error = Tuple[float, float, float]
 
 def main():
     print(
@@ -13,15 +13,15 @@ def main():
         "\nPara introducir las ecuaciones, " +
         "ingrese los índices separados por espacios."
     )
-    # (string, string, string) -> (float, float, float)
-    toRow = lambda x: (float(x[0]), float(x[1]), float(x[2]), float(x[3]))
+    toRow: Callable[[List[str]], Row] = \
+        lambda x: (float(x[0]), float(x[1]), float(x[2]), float(x[3]))
     row_1 = toRow(input("Inserte los índices de la ecuación 1: ").split(" "))
     row_2 = toRow(input("Inserte los índices de la ecuación 2: ").split(" "))
     row_3 = toRow(input("Inserte los índices de la ecuación 3: ").split(" "))
     m = (row_1, row_2, row_3)
     print("Su matriz:",
         "\n[", m[0][0], m[0][1], m[0][2], "|", m[0][3], "]",
-        "\n[", m[1][0], m[1][1], m[1][2], "|", m[1][3], "]"
+        "\n[", m[1][0], m[1][1], m[1][2], "|", m[1][3], "]",
         "\n[", m[2][0], m[2][1], m[2][2], "|", m[2][3], "]")
     if not diagonallyDominant(m):
         print("La matriz ingresada no es diagonalmente dominante")
@@ -39,22 +39,23 @@ def main():
 
 def gaussSeidel(expectedError: float, m: Matrix) -> Results:
     approximate = mkApproximate(m)
-    previous = approximate((0, 0, 0))
-    current = approximate(previous)
-    error = getError(previous, current)
-    # El código descomentado te permite ver las iteraciones paso a paso
-    # print("0: ", previous, current, error)
-    # i = 0
-    while not withinError(expectedError, error):
+    current = (0, 0, 0)
+    i = 1
+    while True:
         previous = current
         current = approximate(current)
         error = getError(previous, current)
-        # i += 1
-        # print(str(i) + ": ", previous, current, error)
-        # sleep(1)
+        print(f"${i}: "
+            f"Previous: (${previous[0]:.4f}, ${previous[1]:.4f}, ${previous[2]:.4f})"
+            f"Current: (${current[0]:.4f}, ${current[1]:.4f}, ${current[2]:.4f})" 
+            f"Error: (${error[0]:.4f}, ${error[1]:.4f}, ${error[2]:.4f})"
+        )
+        sleep(1)
+        i += 1
+        if withinError(expectedError, error): break
     return current
 
-def mkApproximate(m: Matrix) -> Callable[[tuple[float, float, float]], float]:
+def mkApproximate(m: Matrix) -> Callable[[Results], Results]:
     def approximate(x: Results):
         x_1 = (m[0][3] - m[0][1]*x[1] - m[0][2]*x[2]) / m[0][0]
         x_2 = (m[1][3] - m[1][0]*x_1  - m[1][2]*x[2]) / m[1][1]
@@ -63,22 +64,20 @@ def mkApproximate(m: Matrix) -> Callable[[tuple[float, float, float]], float]:
     return approximate
 
 def getError(current_iter: Results, prev_iter: Results) -> Error:
-    e_1 = abs((current_iter[0] - prev_iter[0]) / current_iter[0]) * 100
-    e_2 = abs((current_iter[1] - prev_iter[1]) / current_iter[1]) * 100
-    e_3 = abs((current_iter[2] - prev_iter[2]) / current_iter[2]) * 100
-    return (e_1, e_2, e_3)
+    return (
+        abs((current_iter[0] - prev_iter[0]) / current_iter[0]) * 100,
+        abs((current_iter[1] - prev_iter[1]) / current_iter[1]) * 100,
+        abs((current_iter[2] - prev_iter[2]) / current_iter[2]) * 100)
 
-def withinError(expectedError: float, error: float) -> bool:
-    isWithinExpected_1 = error[0] < expectedError
-    isWithinExpected_2 = error[1] < expectedError
-    isWithinExpected_3 = error[2] < expectedError
-    return isWithinExpected_1 and isWithinExpected_2 and isWithinExpected_3
+def withinError(expectedError: float, error: Error) -> bool:
+    return error[0] <= expectedError and \
+           error[1] <= expectedError and \
+           error[2] <= expectedError
 
 def diagonallyDominant(m: Matrix) -> bool:
-    isDominant_1 = m[0][0] > m[0][1] and m[0][0] > m[0][2]
-    isDominant_2 = m[1][1] > m[1][0] and m[1][1] > m[1][2]
-    isDominant_3 = m[2][2] > m[2][0] and m[2][2] > m[2][1]
-    return isDominant_1 and isDominant_2 and isDominant_3
+    return m[0][0] > m[0][1] and m[0][0] > m[0][2] and \
+           m[1][1] > m[1][0] and m[1][1] > m[1][2] and \
+           m[2][2] > m[2][0] and m[2][2] > m[2][1]
 
 if(__name__ == "__main__"):
     main()
