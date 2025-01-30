9f7cb5e9d81c4841ff0956bc44fa45a8c9481450
Julian Salazar <julian.salazarlpz@proton.me>
2025-01-30 22:01:17 +0000

    More formatting changes

--- gauss-seidel.py
+++ gauss-seidel.py
@@ -8,9 +8,9 @@ type Error = Tuple[float, float, float]
 
 def main():
     print(
-        "# Método Gauss-Seidel para sistemas de ecuaciones que forman" +
-        "una matriz diagonalmente dominante. #" +
-        "\nPara introducir las ecuaciones, " +
+        "# Método Gauss-Seidel para sistemas de ecuaciones que forman"
+        "una matriz diagonalmente dominante. #"
+        "\nPara introducir las ecuaciones, "
         "ingrese los índices separados por espacios."
     )
     toRow: Callable[[List[str]], Row] = \
@@ -19,23 +19,19 @@ def main():
     row_2 = toRow(input("Inserte los índices de la ecuación 2: ").split(" "))
     row_3 = toRow(input("Inserte los índices de la ecuación 3: ").split(" "))
     m = (row_1, row_2, row_3)
-    print("Su matriz:",
-        "\n[", m[0][0], m[0][1], m[0][2], "|", m[0][3], "]",
-        "\n[", m[1][0], m[1][1], m[1][2], "|", m[1][3], "]",
-        "\n[", m[2][0], m[2][1], m[2][2], "|", m[2][3], "]")
+    print("Su matriz:"
+        f"\n[ {m[0][0]} {m[0][1]} {m[0][2]} | {m[0][3]} ]"
+        f"\n[ {m[1][0]} {m[1][1]} {m[1][2]} | {m[1][3]} ]"
+        f"\n[ {m[2][0]} {m[2][1]} {m[2][2]} | {m[2][3]} ]")
     if not diagonallyDominant(m):
         print("La matriz ingresada no es diagonalmente dominante")
         return
     expectedError = float(input("Ingrese el error esperado: "))
     result = gaussSeidel(expectedError, m)
-    print("Resultado: ",
-        "\nx_1: ", result[0],
-        "\nx_2: ", result[1],
-        "\nx_3: ", result[2])
-    print(
-        "eq1: ",   m[0][0]*result[0] + m[0][1]*result[1] + m[0][2]*result[2], "=", m[0][3],
-        "\neq2: ", m[1][0]*result[0] + m[1][1]*result[1] + m[1][2]*result[2], "=", m[1][3],
-        "\neq3: ", m[2][0]*result[0] + m[2][1]*result[1] + m[2][2]*result[2], "=", m[2][3])
+    print(f"Resultado: ({result[0]:.4f}, {result[1]:.4f}, {result[2]:.4f})"
+        f"\neq1: {m[0][0]*result[0] + m[0][1]*result[1] + m[0][2]*result[2]:.4f} = {m[0][3]}"
+        f"\neq2: {m[1][0]*result[0] + m[1][1]*result[1] + m[1][2]*result[2]:.4f} = {m[1][3]}"
+        f"\neq3: {m[2][0]*result[0] + m[2][1]*result[1] + m[2][2]*result[2]:.4f} = {m[2][3]}")
 
 def gaussSeidel(expectedError: float, m: Matrix) -> Results:
     approximate = mkApproximate(m)
@@ -45,10 +41,10 @@ def gaussSeidel(expectedError: float, m: Matrix) -> Results:
         previous = current
         current = approximate(current)
         error = getError(current, previous)
-        print(f"${i}: "
-            f"Previous: (${previous[0]:.4f}, ${previous[1]:.4f}, ${previous[2]:.4f})"
-            f"Current: (${current[0]:.4f}, ${current[1]:.4f}, ${current[2]:.4f})" 
-            f"Error: (${error[0]:.4f}, ${error[1]:.4f}, ${error[2]:.4f})"
+        print(f"{i}: "
+            f"Previo: ({previous[0]:.4f}, {previous[1]:.4f}, {previous[2]:.4f}) "
+            f"Actual: ({current[0]:.4f}, {current[1]:.4f}, {current[2]:.4f}) " 
+            f"Error: ({error[0]:.4f}, {error[1]:.4f}, {error[2]:.4f})"
         )
         sleep(1)
         i += 1
