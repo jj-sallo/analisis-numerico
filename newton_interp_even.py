71f94a082cac0cedf186a0939f85b8149790b1ed
Julian Salazar <julian.salazarlpz@proton.me>
2025-03-13 21:04:43 +0000

    Finished newton_interp_even.py
    - Prettyfied some comments
    - Deleted non-math comments
    - Added console input
    - Factorized code
    - Fixed type annotations
    - Deleted range checks on recrusive functions

--- newton_interp_even.py
+++ newton_interp_even.py
@@ -1,80 +1,104 @@
-from collections.abc import Iterable
 from functools import cache
-from operator import mul
 from math import isclose
 from math import factorial as fact
 
-# Para cualquier conjunto y_1, y_2, ..., y_i, ..., y_n
-# el operador de diferencia Δ^k a_i se define como
+# Para cualquier conjunto de tamaño n
+#   y₀, y₁, y₂, ..., yᵢ, ..., yₙ
+# el operador de diferencia Δᵏaᵢ se define como
 #
-#           | 1                               si k = 0
-# Δ^k a_i = | y_{i+1} - y_i                   si k = 1
-#           | Δ^{k-1} y_{i+1} + Δ^{k-1} y_{i} si k > 1
+#        | 1                 si k = 0
+# Δᵏyᵢ = | yᵢ₊₁ - yᵢ         si k = 1
+#        | Δᵏ⁻¹yᵢ₊₁ + Δᵏ⁻¹yᵢ si k > 1
 #
-# donde n >= 2
-#       0 <= k < n - 1
-#       0 <= i < n - k - 1
-def mkDifference(ys: [float]):
-    # Tenemos que crear esta función que retorna otra función
-    # porque @cache sólo puede actuar sobre valores hasheables,
-    # y las listas no son hasheables.
-
-    n = len(ys)
-    # @cache va a guardar los valores previamente calculados
-    # tal que, si previamente calculamos delta(3, 0), ya no tenemos
-    # que calcularlo de nuevo y nos ahorramos su cómputo
+# donde n ≥ 1
+#       0 ≤ k < n
+#       0 ≤ i < n - k
+#       n, i, k ∈ ℕ
+def mkDifference(ys: list[float]):
     @cache
     def difference(k: int, i: int) -> float:
-        if   n < 2:
-            raise ValueError("n debe ser mayor a 2")
-        elif k < 0 or k > n - 1:
-            raise ValueError("k debe estar en el rango 0 <= k < n - 1")
-        elif i < 0 or i > n - k - 1:
-            raise ValueError("i debe estar en el rango 0 <= i < n - k - 1")
-        elif k == 0:
-            return ys[0]
-        elif k == 1:
-            return ys[i + 1] - ys[i]
-        else: # i > 0, k > 1
-            return difference(k - 1, i + 1) - difference(k - 1, i)
+        if   k == 0: return ys[0]
+        elif k == 1: return ys[i + 1] - ys[i]
+        else:        return difference(k - 1, i + 1) - difference(k - 1, i)
     return difference
 
-# Para cualquier conjunto x_0, x_1, ..., x_N,
+# Para cualquier conjunto de tamaño n
+#   x₀, x₁, ..., xⱼ, ..., xₙ
 # la base de Newton se define de la siguiente manera
 #
-# n_j(x) = | 1                           si j = 0
-#          | Π^(j-1)_{i=0} (x - x_i)     si j > 0
+# nⱼ(x) = | 1                    si j = 0
+#         | Πʲ⁻¹ᵢ₌₀ (x - xᵢ)     si j > 0
 #
 # Esto se puede reescribir de forma recursiva como
 #
-# n_j(x) = | 1                           si j = 0
-#          | (x - x_{j-1}) * n_{j-1}(x)  si j > 0
+# nⱼ(x) = | 1                     si j = 0
+#         | (x - xⱼ₋₁) * nⱼ₋₁(x)  si j > 0
 #
-# donde N >= 2
-#       j <= N
-def mkNewtonBasis(xs: [float]):
-    n = len(xs)
+# donde n ≥ 0
+#       0 ≤ j < n
+#       n, j ∈ ℕ
+def mkNewtonBasis(xs: list[float]):
     @cache
-    def newtonBasis(x: float, i: float) -> float:
-        if   i < 0:  raise ValueError("i no puede ser menor a 0")
-        elif i > n:  raise ValueError("i no puede ser mayor a n")
-        elif i == 0: return 1
-        else:        return (x - xs[i - 1]) * newtonBasis(x, i - 1)
+    def newtonBasis(x: float, j: int) -> float:
+        if j == 0: return 1
+        else:      return (x - xs[j - 1]) * newtonBasis(x, j - 1)
     return newtonBasis
 
-def mkNewtonPolynomial(xs: [float], ys: [float]):
+def mkNewtonPolynomial(xs: list[float], ys: list[float]):
     n = len(xs)
     spacing = abs(xs[0] - xs[1])
-    haveSameSpacing = \
-        [isclose(abs(xs[i] - xs[i + 1]), spacing) for i in range(1, n - 1)]
-    if not all(haveSameSpacing):
-        raise ValueError(
-            "Los valores de x tienen que tener el mismo espaciado entre ellos"
-        )
     difference = mkDifference(ys)
     basis = mkNewtonBasis(xs)
-    def newtonPolynomial(x) -> float:
+    def newtonPolynomial(x: float) -> float:
         def term(i: int) -> float:
             return difference(i, 0) * basis(x, i) / (fact(i) * spacing ** i)
         return sum(term(i) for i in range(n))
     return newtonPolynomial
+
+def sameSpacingBetween(xs: list[float]) -> bool:
+    n = len(xs)
+    spacing = abs(xs[0] - xs[1])
+    haveSameSpacing = [isclose(abs(xs[i] - xs[i + 1]), spacing) for i in range(1, n - 1)]
+    return all(haveSameSpacing)
+
+def parsePoints() -> tuple[list[float], list[float]]:
+    i = 1
+    xs: list[float] = []
+    ys: list[float] = []
+    print(
+        "Ingrese los puntos a interpolar."
+        "\nCada punto debe consistir en dos números separados por espacios."
+    )
+    while True:
+        s = input(f"{i}: ").strip()
+        if s == "END": return (xs, ys)
+        try:
+            first, second = s.split(" ")
+            xs.append(float(first))
+            ys.append(float(second))
+            i += 1
+        except ValueError:
+            print("Se insertaron valores inválidos, intente de nuevo.")
+
+def parseX() -> float:
+    print("Ingrese el valor de x a aproximar")
+    while True:
+        try:    return float(input("x: "))
+        except: print("El valor ingresado no es válido, intente de nuevo.")
+
+def main():
+    xs, ys = parsePoints()
+    if not sameSpacingBetween(xs):
+        print("Los valores de x tienen que tener el mismo espaciado entre ellos")
+        return
+    if len(xs) == 0:
+        print("No se ingresaron puntos")
+        return
+    x = parseX()
+    newtonPolynomial = mkNewtonPolynomial(xs, ys)
+    y = newtonPolynomial(x)
+    print(f"Valor obtenido: {y}")
+    print(f"Nuevo punto: ({x}, {y:.4f})")
+
+if __name__ == "__main__":
+    main()
